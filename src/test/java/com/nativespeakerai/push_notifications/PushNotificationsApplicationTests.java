5f79e158f76470073ac25ce6c2ace2b9a5a7f881
Juan-Fruto <jfruto@ucol.mx>
2024-02-08 02:06:27 -0600

    Initial commit

---
+++ src/test/java/com/nativespeakerai/push_notifications/PushNotificationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nativespeakerai.push_notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PushNotificationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
